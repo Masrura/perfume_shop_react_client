c268a17566a9609d43f0f1fa60b15d80ab3d2003
Masrura <masrura.uddin96@gmail.com>
2021-11-10 08:12:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthProvider from './context/AuthProvider';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import NotFound from './component/NotFound/NotFound';
+import Login from './component/Login/Login';
+import Private from './component/Private/Private'
+import PrivateRoute from './component/PrivareRoute/PrivateRoute';
+import Register from './component/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route  path="/home">
+              <Home></Home>
+            </Route>
+            <PrivateRoute path="/appointment">
+              <Private></Private>
+            </PrivateRoute>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/register">
+              <Register></Register>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
